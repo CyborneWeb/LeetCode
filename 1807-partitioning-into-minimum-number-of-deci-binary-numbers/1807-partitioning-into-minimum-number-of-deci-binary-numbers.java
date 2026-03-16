c9afe7d1733b4c283fed36ed5db10cb631b406d3
Matic Štucin <matic.stucin@gmail.com>
2026-03-16 18:57:58 +0100

    1807-PARTITIONING-INTO-MINIMUM-NUMBER-OF-DECI-BINARY-NUMBERS Stats: Time: 35 ms (5.09%), Space: 47.6 MB (36.93%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,8 +1,14 @@
 class Solution {
     public int minPartitions(String n) {
         int max = 0;
+
+        if (n.length() == 1){
+            return Integer.valueOf(n);
+        }
+
+
         for (int i = 0; i<n.length(); i++){
-            int current = Integer.parseInt(String.valueOf(n.charAt(i)));
+            int current = Integer.valueOf(String.valueOf(n.charAt(i)));
             if (current > max){
                 max = current;
             }
